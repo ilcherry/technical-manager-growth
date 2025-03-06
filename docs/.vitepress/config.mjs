212502c118c589c292d7eabbba74420cda7e38db
ilcherry <1062217965@qq.com>
2025-03-06 20:55:09 +0800

    初始化 VitePress 文档站点，添加基本配置和部署工作流

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,35 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "技术经理成长之路",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    // nav: [
+    //   { text: "Home", link: "/" },
+    //   { text: "我是什么样的人", link: "/我是什么样的人" },
+    // ],
+
+    sidebar: [
+      {
+        text: "Examples",
+        items: [
+          { text: "我是什么样的人", link: "/我是什么样的人" },
+          { text: "发现团队的瓶颈", link: "/发现团队的瓶颈" },
+          { text: "处理线上问题", link: "/处理线上问题" },
+          { text: "大重构", link: "/大重构" },
+          { text: "要懂一些项目管理的知识", link: "/要懂一些项目管理的知识" },
+          {
+            text: "对自己开发的模块要认真负责",
+            link: "/对自己开发的模块要认真负责",
+          },
+        ],
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,11 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "技术经理成长之路"
+  actions:
+    - theme: brand
+      text: 点击阅读
+      link: /markdown-examples
+---
